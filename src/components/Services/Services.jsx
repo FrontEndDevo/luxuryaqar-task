a71083cfa3324f20e5fc70ffef4f5a78802257e5
FrontEndDevo <mylinkacco@gmail.com>
2023-07-23 19:37:42 +0300

    Finish the structure of JSX in Services component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,6 +1,26 @@
+import SingleItem from "../SingleItem/SingleItem";
+import Title from "../Title/Title";
 import classes from "./Services.module.scss";
 const Services = (props) => {
-  return <section className={classes.services}>Services</section>;
+  const allServices = props.services.map((service, index) => (
+    <SingleItem
+      key={index}
+      img={service.img}
+      title={service.title}
+      description={service.description}
+      time={service.time}
+      cost={service.cost}
+    />
+  ));
+  return (
+    <section className={classes.services}>
+      <Title
+        title="Services"
+        description="We offer a wide range of services for you"
+      />
+      <ul className={classes["all-services"]}>{allServices}</ul>
+    </section>
+  );
 };
 
 export default Services;
