e511d48f49058e2623fdf5e31c3aa7562e60f1ec
FrontEndDevo <mylinkacco@gmail.com>
2023-07-23 15:06:29 +0300

    Create Offers section and change the application background

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "@/components/Header/Header";
 import Navbar from "@/components/Navbar/Navbar";
+import Offers from "@/components/Offers/Offers";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Header />
+      <Offers />
     </>
   );
 }
---
+++ src/components/Offers/Offers.jsx
@@ -0,0 +1,9 @@
+import classes from './Offers.module.scss';
+
+const Offers = () => {
+  return (
+    <section className={classes.offers}>Offers</section>
+  )
+}
+
+export default Offers
