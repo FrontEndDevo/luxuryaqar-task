92c4430648735f3be34e5d0d1c661d1d5866abcd
FrontEndDevo <mylinkacco@gmail.com>
2023-07-23 15:27:48 +0300

    Set the main JSX of Offers component

--- src/components/Offers/Offers.jsx
+++ src/components/Offers/Offers.jsx
@@ -1,9 +1,22 @@
-import classes from './Offers.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classes from "./Offers.module.scss";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Offers = (props) => {
   return (
-    <section className={classes.offers}>Offers</section>
-  )
-}
+    <section className={classes.offers}>
+      <div className={classes.title}>
+        <div className={classes.introduction}>
+          <h2>Offers</h2>
+          <p>Promotions, deals and special offers for you</p>
+        </div>
+        <button>
+          View all
+          <FontAwesomeIcon icon={faArrowRight} />
+        </button>
+      </div>
+    </section>
+  );
+};
 
-export default Offers
+export default Offers;
