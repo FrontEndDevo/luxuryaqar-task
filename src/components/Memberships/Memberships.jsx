bc53ed2e18f7a861c117f572adbbb2d5200d7d5f
FrontEndDevo <mylinkacco@gmail.com>
2023-07-23 23:15:25 +0300

    Add and style the title of the Memberships component

--- src/components/Memberships/Memberships.jsx
+++ src/components/Memberships/Memberships.jsx
@@ -1,6 +1,13 @@
 import classes from "./Memberships.module.scss";
 const Memberships = (props) => {
-  return <section className={classes.memberships}>Memberships</section>;
+  return (
+    <section className={classes.memberships}>
+      <div className={classes.title}>
+        <h2>Memberships</h2>
+        <p>At vero eos et accusamus et iust deleniti atque corrupti quos.</p>
+      </div>
+    </section>
+  );
 };
 
 export default Memberships;
