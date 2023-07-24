069de14a956115ee1fbdbfacd2fab8bf4f3347eb
FrontEndDevo <mylinkacco@gmail.com>
2023-07-24 23:51:50 +0300

    Set the active class for (first element) in Bullet component

--- src/components/Reviews/Bullet/Bullet.jsx
+++ src/components/Reviews/Bullet/Bullet.jsx
@@ -1,6 +1,10 @@
 import classes from "./Bullet.module.scss";
 const Bullet = (props) => {
-  return <span className={classes.bullet}></span>;
+  return (
+    <span
+      className={`${classes.bullet} ${props.index == 1 ? classes.active : ""}`}
+    ></span>
+  );
 };
 
 export default Bullet;
