bda9d18850e913449de4dbe18db1ecc9fcd8e4af
FrontEndDevo <mylinkacco@gmail.com>
2023-07-24 22:40:12 +0300

    Finish the JSX of Review component

--- src/components/Reviews/Review/Review.jsx
+++ src/components/Reviews/Review/Review.jsx
@@ -1,9 +1,21 @@
-import classes from './Review.module.scss';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classes from "./Review.module.scss";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 const Review = (props) => {
-
+  const { rate, feedback, reviewer } = props;
   return (
-    <li className={classes.review}>Review</li>
-  )
-}
+    <li className={classes.review}>
+      <div className={classes.rate}>
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+        <FontAwesomeIcon icon={faStar} />
+      </div>
+      <div className={classes.feedback}>"{feedback}"</div>
+      <div className={classes.reviewer}>_ By {reviewer}</div>
+    </li>
+  );
+};
 
-export default Review
+export default Review;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,10 +1,18 @@
+import Review from "./Review/Review";
 import classes from "./Reviews.module.scss";
 const Reviews = (props) => {
-  
+  const allReviews = props.reviews.map((review, index) => (
+    <Review
+      key={index}
+      rate={review.rate}
+      feedback={review.feedback}
+      reviewer={review.reviewer}
+    />
+  ));
   return (
     <section className={classes.reviews}>
       <h2 className={classes.title}>What they say about us</h2>
-      <ul className={classes['all-reviews']}></ul>
+      <ul className={classes["all-reviews"]}>{allReviews}</ul>
     </section>
   );
 };
