c80f93b224103b8baae4625e8b177eff0131fb1f
FrontEndDevo <mylinkacco@gmail.com>
2023-07-24 22:31:05 +0300

    Put the basic structure (JSX) of Reviews component

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,12 @@
 import classes from "./Reviews.module.scss";
 const Reviews = (props) => {
-  return <section className={classes.reviews}>Reviews</section>;
+  
+  return (
+    <section className={classes.reviews}>
+      <h2 className={classes.title}>What they say about us</h2>
+      <ul className={classes['all-reviews']}></ul>
+    </section>
+  );
 };
 
 export default Reviews;
